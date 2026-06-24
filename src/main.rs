c33443c9e5ffc47910fa131ff274336b91f60966
AzureZee <azureze@qq.com>
2026-06-25 07:13:30 +0800

    added modify path flags: [append/prepend/delete/edit-path]

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,119 @@
-use std::{env, io};
-use winreg::{HKCU, RegKey, enums::KEY_SET_VALUE};
+use std::{
+    env,
+    fs::{File, read_to_string, remove_file},
+    io::{self, Write},
+    process::Command,
+};
+use winreg::{HKCU, RegKey, enums::*};
 
 type IoResult<T> = Result<T, io::Error>;
 
+// setx <name> [value]
+// setx -[(a|append)|(p|prepend)|(m|move)|(d|delete)] <paths...>
+// setx -[e|edit-path] <editor>
+macro_rules! env_set_value {
+    () => {
+        HKCU.open_subkey_with_flags(ENVIRONMENT, KEY_SET_VALUE)
+    };
+}
 fn main() -> IoResult<()> {
-    let mut args = env::args().skip(1);
-    match (&args.next(),&args.next()) {
+    let mut args = env::args().skip(1).peekable();
+    match (&args.next(), args.peek()) {
+        (Some(flag), Some(_)) if flag.starts_with("-") => {
+            let cu_env = HKCU.open_subkey_with_flags(ENVIRONMENT, KEY_ALL_ACCESS)?;
+            let args: Vec<_> = args.collect();
+            let flag = flag.trim_start_matches("-");
+            set_path(args, flag, cu_env)?;
+        }
         (Some(name), value) => {
             if let Some(value) = value {
                 set_var(name, value)?
-            }else {
+            } else {
                 remove_var(name)?
             }
-        },
+        }
         _ => todo!(),
     }
     Ok(())
 }
+
+fn set_path(args: Vec<String>, flag: &str, cu_env: RegKey) -> IoResult<()> {
+    let mut path_var: String = cu_env.get_value(PATH)?;
+
+    const SEMICOLON: &str = ";";
+    const LF: &str = "\n";
+
+    macro_rules! set_path_val {
+        ($val:tt) => {
+            cu_env.set_value(PATH, &$val)?;
+            unsafe { env::set_var(PATH, &$val) };
+            
+        };
+    }
+    Ok(match flag {
+        "a" | "append" => {
+            let path_args = args.join(SEMICOLON);
+            if !path_var.ends_with(SEMICOLON) {
+                path_var.push_str(SEMICOLON);
+            }
+            path_var.push_str(&path_args);
+            set_path_val!(path_var);
+        }
+        "p" | "prepend" => {
+            let mut path_args = args.join(SEMICOLON);
+            path_args.push_str(SEMICOLON);
+            path_args.push_str(&path_var);
+            set_path_val!(path_args);
+        }
+        "d" | "delete" => {
+            let new = path_var
+                .split(SEMICOLON)
+                .filter(|p| !p.is_empty())
+                .filter_map(|p| (!args.iter().any(|a| a == p)).then_some(p))
+                .collect::<Vec<_>>()
+                .join(SEMICOLON);
+            set_path_val!(new);
+        }
+        "e" | "edit-path" => {
+            let tmp_file_path = env::temp_dir().join("edit-path_xxxxxx.txt");
+            if tmp_file_path.exists() {
+                remove_file(&tmp_file_path)?;
+            }
+            let mut file = File::create_new(&tmp_file_path)?;
+            let buf = path_var.replace(SEMICOLON, LF);
+            file.write_all(buf.as_bytes())?;
+
+            let mut editor: &str = &args[0];
+            if editor == "code" {
+                editor = "code.cmd";
+            }
+            let mut cmd = Command::new(editor);
+            if ["code.cmd", "zed"].contains(&editor) {
+                cmd.arg("--wait");
+            }
+            cmd.arg(&tmp_file_path).spawn()?.wait()?;
+
+            let new = read_to_string(&tmp_file_path)?;
+            let new = new.trim_end().replace(LF, SEMICOLON);
+            set_path_val!(new);
+
+            remove_file(tmp_file_path)?;
+        }
+        _ => {}
+    })
+}
+
 fn set_var(name: &str, value: &str) -> IoResult<()> {
-    let cu_env = HKCU.open_subkey_with_flags(ENVIRONMENT, KEY_SET_VALUE)?;
+    let cu_env = env_set_value!()?;
     cu_env.set_value(name, &value)?;
     unsafe { env::set_var(name, value) };
     Ok(())
 }
 fn remove_var(name: &str) -> IoResult<()> {
-    let cu_env = HKCU.open_subkey_with_flags(ENVIRONMENT, KEY_SET_VALUE)?;
+    let cu_env = env_set_value!()?;
     cu_env.delete_value(name)?;
     unsafe { env::remove_var(name) };
     Ok(())
 }
-fn path_vec(cu_env: &RegKey) -> Vec<String> {
-    let path: String = cu_env.get_value(PATH).unwrap();
-    let path: Vec<_> = path
-        .split(";")
-        .filter_map(|p| (!p.is_empty()).then_some(p.to_string()))
-        .collect();
-    path
-}
 const ENVIRONMENT: &str = "Environment";
 const PATH: &str = "Path";
-// struct Env {
-//     name: String,
-//     value: String,
-// }
-// fn env_iter(cu_env: &RegKey) -> impl Iterator<Item = Env> {
-//     cu_env.enum_values().filter_map(|v| v.ok()).map(|v| Env {
-//         name: v.0,
-//         value: format!("{}", v.1),
-//     })
-// }
