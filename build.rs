e1925e4378e002fe8defa6d759f6999d1a0416e4
AzureZee <azureze@qq.com>
2026-06-28 20:39:17 +0800

    fix windows build failed

--- build.rs
+++ build.rs
@@ -6,25 +6,25 @@ use std::{
 fn main() -> io::Result<()> {
     let root = env::var("CARGO_MANIFEST_DIR").unwrap();
     let mut profile = env::var("PROFILE").unwrap();
-    if let Ok(target) = env::var("TARGET") {
+    if cfg!(unix) {
+        let target = env::var("TARGET").unwrap();
         profile = format!("{target}/{profile}");
     }
     let out = format!("{root}/target/{profile}");
-    // let out = Path::new(&out);
-    set_current_dir(out)?;
-    dbg!(env::current_dir()?);
+    set_current_dir(out).unwrap();
+    dbg!(env::current_dir().unwrap());
     let original = "setv.exe";
     let link = "setm.exe";
 
     #[cfg(windows)]
     {
         dbg!("windows");
-        std::os::windows::fs::symlink_file(original, link)?;
+        std::os::windows::fs::symlink_file(original, link).unwrap();
     }
     #[cfg(unix)]
     {
         dbg!("unix");
-        std::os::unix::fs::symlink(original, link)?;
+        std::os::unix::fs::symlink(original, link).unwrap();
     }
     println!("cargo::rerun-if-changed=build.rs");
     Ok(())
